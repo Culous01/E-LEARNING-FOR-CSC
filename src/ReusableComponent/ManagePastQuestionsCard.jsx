591b4e80f17ddddd5e82863b02c769e013c7b06f
name <adekunlemichael198@gmail.com>
2025-09-03 22:24:59 +0100

    Correction...

--- src/ReusableComponent/ManagePastQuestionsCard.jsx
+++ src/ReusableComponent/ManagePastQuestionsCard.jsx
@@ -1,11 +1,9 @@
-import PastQuestionsImg from '../Assets/PastQuestionsImg.jpg';
-
 export const ManagePastQuestionsCard = ({pastQuestionsImg, courseTitle, level, semester}) => {
     return (
         <div className="grid grid-cols-2 lg:gap-10 md:gap-10 gap-5">
             <div className='flex flex-col justify-center space-y-5 shadow-xl rounded-2xl lg:p-5 p-3.5'>
                 <div className="w-full aspect-[16/10] overflow-hidden">
-                    <img src={PastQuestionsImg} className='rounded-xl w-full h-full object-cover'  alt="PastQuestionsImg"/>
+                    <img src={pastQuestionsImg} className='rounded-xl w-full h-full object-cover'  alt="PastQuestionsImg"/>
                 </div>
 
                 <div className="flex flex-col lg:space-y-2 md:space-y-2 space-y-0.5 mb-3">
